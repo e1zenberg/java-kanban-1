99bb1b17fef06123b5f024031aa4a8f5e1d901a9
e1zenberg <e1zenbergdev@gmail.com>
2025-05-01 20:22:52 +0300

    Исправления 9 и 10 строчек

--- src/managers/HistoryManager.java
+++ src/managers/HistoryManager.java
@@ -5,7 +5,10 @@ import managers.task.Task;
 import java.util.List;
 
 public interface HistoryManager {
+
     void add(Task task);
+
     void remove(int id); // добавили метод
+
     List<Task> getHistory();
 }
